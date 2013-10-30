2d649a3c837f830cc727dc1f3bddf5be0714ea0c
flinx777 <flinx777@gmail.com>
2013-10-30 13:23:56 -0400

    update

--- assets/templates/project-name/modx/chunks/footer_js.php
+++
@@ -1,3 +0,0 @@
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="assets/templates/primathal/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="assets/templates/project-name/js/main.js"></script>
